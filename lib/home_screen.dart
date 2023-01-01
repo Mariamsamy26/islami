1aee60394c6216aeec1cc237d01872f991c0b235
islami <https://github.com/Mariamsamy26/islami.git>
2023-01-01 18:43:28 +0200

    create app git into

---
+++ lib/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class homeScreen extends StatelessWidget {
+  static const String rountName ='home';
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('islami'),
+      ),
+    );
+  }
+}
